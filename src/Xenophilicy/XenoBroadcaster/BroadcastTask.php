6b6dbc652de51bc6d751124279030100d745a1c3
Ｄａｒｋｎｅｓｓ <135982991+darknesslocalhost@users.noreply.github.com>
2023-11-08 12:54:28 +0100

    Update BroadcastTask.php

--- src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
+++ src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
@@ -6,19 +6,19 @@ use pocketmine\scheduler\Task;
 class BroadcastTask extends Task {
     private $plugInstance;
 
-    public function onRun(int $currentTick): void {
-        $this->plugInstance = XenoBroadcaster::getInstance();
-        $prefix = str_replace("&", "§", $this->plugInstance->getConfig()->get("Message-Prefix"));
-        $messages = $this->plugInstance->getConfig()->get("Messages");
-        $message = $messages[array_rand($messages)];
-        $server = $this->plugInstance->getServer();
+public function onRun(): void {
+    $this->plugInstance = XenoBroadcaster::getInstance();
+    $prefix = str_replace("&", "§", $this->plugInstance->getConfig()->get("Message-Prefix"));
+    $messages = $this->plugInstance->getConfig()->get("Messages");
+    $message = $messages[array_rand($messages)];
+    $server = $this->plugInstance->getServer();
 
-        $message = str_replace(
-            ["&", "{break}", "{online}", "{max}", "{motd}", "{tps}", "{api}"],
-            ["§", "\n", count($server->getOnlinePlayers()), $server->getMaxPlayers(), $server->getMotd(), $server->getTicksPerSecond(), $server->getVersion()],
-            $message
-        );
+    $message = str_replace(
+        ["&", "{break}", "{online}", "{max}", "{motd}", "{tps}", "{api}"],
+        ["§", "\n", count($server->getOnlinePlayers()), $server->getMaxPlayers(), $server->getMotd(), $server->getTicksPerSecond(), $server->getVersion()],
+        $message
+    );
 
-        $this->plugInstance->broadcastMessage($prefix . $message);
+    $this->plugInstance->broadcastMessage($prefix . $message);
     }
 }
