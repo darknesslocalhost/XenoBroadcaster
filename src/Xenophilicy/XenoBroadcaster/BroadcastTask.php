d13ff06bfe9edbf7d5644243fd7ff95e3ae9ed08
Ｄａｒｋｎｅｓｓ <135982991+darknesslocalhost@users.noreply.github.com>
2023-11-08 12:46:10 +0100

    Update BroadcastTask.php

--- src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
+++ src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
@@ -6,11 +6,11 @@ use pocketmine\scheduler\Task;
 class BroadcastTask extends Task {
     private $plugInstance;
 
-    public function onRun(int $currentTick) : void {
+    public function onRun(int $currentTick): void {
         $this->plugInstance = XenoBroadcaster::getInstance();
         $prefix = str_replace("&", "§", $this->plugInstance->getConfig()->get("Message-Prefix"));
         $messages = $this->plugInstance->getConfig()->get("Messages");
-        $message = $messages[array_rand($messages]);
+        $message = $messages[array_rand($messages)];
         $server = $this->plugInstance->getServer();
 
         $message = str_replace(["&", "{break}", "{online}", "{max}", "{motd}", "{tps}", "{api}"], ["§", "\n", count($server->getOnlinePlayers()), $server->getMaxPlayers(), $server->getMotd(), $server->getTicksPerSecond(), $server->getVersion()], $message);
