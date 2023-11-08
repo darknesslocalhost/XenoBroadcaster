1cf2e5e1277eda5edd73de93149c4aeb5d119f74
Ｄａｒｋｎｅｓｓ <135982991+darknesslocalhost@users.noreply.github.com>
2023-11-08 12:43:30 +0100

    Update BroadcastTask.php

--- src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
+++ src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
@@ -11,14 +11,10 @@ class BroadcastTask extends Task {
         $prefix = str_replace("&", "§", $this->plugInstance->getConfig()->get("Message-Prefix"));
         $messages = $this->plugInstance->getConfig()->get("Messages");
         $message = $messages[array_rand($messages)];
-        $server = $this->plugInstance->getServer(); // Pobieranie obiektu serwera
-        $message = str_replace("&", "§", $message);
-        $message = str_replace("{break}", "\n", $message);
-        $message = str_replace("{online}", count($server->getOnlinePlayers()), $message);
-        $message = str_replace("{max}", $server->getMaxPlayers(), $message);
-        $message = str_replace("{motd}", $server->getMotd(), $message);
-        $message = str_replace("{tps}", $server->getTicksPerSecond(), $message);
-        $message = str_replace("{api}", $server->getVersion());
-        $server->broadcastMessage($prefix . $message);
+        $server = $this->plugInstance->getServer();
+
+        $message = str_replace(["&", "{break}", "{online}", "{max}", "{motd}", "{tps}", "{api}"], ["§", "\n", count($server->getOnlinePlayers()), $server->getMaxPlayers(), $server->getMotd(), $server->getTicksPerSecond(), $server->getVersion()], $message);
+
+        $this->plugInstance->broadcastMessage($prefix . $message);
     }
 }
